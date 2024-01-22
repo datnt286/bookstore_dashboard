45e26f3178c393df1d34a063fbfbd9c990d004d7
datnt286 <datnt286@gmail.com>
2024-01-22 11:06:33 +0700

    fix forgot password

--- resources/views/reset-password/index.blade.php
+++ resources/views/reset-password/index.blade.php
@@ -4,6 +4,6 @@
     @endif
     <h2>Mật khẩu mới của bạn là: <span style="color: red;">{{ $newPassword }}</span></h2>
     <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quis laudantium eos obcaecati amet repellat architecto qui perferendis quo soluta, vel eveniet alias reprehenderit, voluptatem accusamus vitae, odio illo deleniti.
+        Vui lòng đăng nhập với mật khẩu mới sau đó đổi mật khẩu.
     </p>
 </div>
